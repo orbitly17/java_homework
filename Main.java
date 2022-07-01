f33ce510cd9d0cb0d4f6c9a32bb3ce45f153c8f7
orbit <1044523642@qq.com>
2022-07-01 21:44:05 +0800

    雇员类

--- Main.java
+++ Main.java
@@ -1,59 +1,142 @@
 import java.util.*;
 
 public class Main {
-    static public void main(String[] args) {
-        Scanner cin = new Scanner(System.in);
-        String n = cin.nextLine();
-        Employee[] employees = new Employee[Integer.parseInt(n)];
-        for(int i = 0; i < Integer.parseInt(n); i++){
-            String mes = cin.nextLine();
-            String[] mes1 = mes.split(" ");
-            switch(Integer.parseInt(mes1[0])){
-                case 0:
-                    SalaridEmployee employee = new SalaridEmployee(mes1[1], mes1[2], mes1[3]);
-                    employee.setWeeklySalary(Double.parseDouble(mes1[4]));
-                    employees[i] = employee;
-                    break;
-                case 1:
-                    HourlyEmployee employee1 = new HourlyEmployee(mes1[1], mes1[2], mes1[3]);
-                    employee1.setWage(Double.parseDouble(mes1[4]));
-                    employee1.setHours(Double.parseDouble(mes1[5]));
-                    employees[i] = employee1;
-                    break;
-                case 2:
-                    CommisionEmployee employee2 = new CommisionEmployee(mes1[1], mes1[2], mes1[3]);
-                    employee2.setGrossSales(Double.parseDouble(mes1[4]));
-                    employee2.setCommissionRate(Double.parseDouble(mes1[5]));
-                    employees[i] = employee2;
-                    break;
-                case 3:
-                    basePlusCommisionEmployee employee3 = new basePlusCommisionEmployee(mes1[1], mes1[2], mes1[3]);
-                    employee3.setGrossSales(Double.parseDouble(mes1[4]));
-                    employee3.setCommissionRate(Double.parseDouble(mes1[5]));
-                    employee3.setBaseSalary(Double.parseDouble(mes1[6]));
-                    employees[i] = employee3;
-                    break;
-            }
+    static factory fac = new factory();
+
+    static Employee getEmployee( String empSecNum) throws EmployeeException {
+        if(fac.getEmployees().containsKey(empSecNum)){
+            return fac.getEmployees().get(empSecNum);
         }
-        Arrays.sort(employees);
-        String m = cin.nextLine();
-        for(int i = 0; i < Integer.parseInt(m); i++){
-            String test = cin.nextLine();
-            String[] test1 = test.split(" ");
-            if(test1[0].equals("0")){
-                for(int j = 0; j < Integer.parseInt(n); j++){
-                    if(employees[j].getFirstName().equals(test1[1])){
-                        employees[j].toString();
+        else{
+            throw new EmployeeException(1);
+        }
+    }
+
+    static Employee deleteEmployee(String empSecNum) throws EmployeeException {
+        if(!fac.getEmployees().containsKey(empSecNum)){
+            throw new EmployeeException(2);
+        }
+        else{
+            Employee tem = fac.getEmployees().get(empSecNum);
+            fac.getEmployees().remove(empSecNum);
+            return tem;
+        }
+    }
+
+    static Employee addEmployee(Employee emp) throws EmployeeException {
+        if(fac.getEmployees().containsKey(emp.getSocialSecurityNumber())){
+            throw new EmployeeException(3);
+        }
+        else{
+            fac.getEmployees().put(emp.getSocialSecurityNumber(), emp);
+            return emp;
+        }
+    }
+
+    static Employee updateEmployee(String empSecNum ,Employee emp) throws EmployeeException {
+        if(!fac.getEmployees().containsKey(empSecNum)){
+            throw new EmployeeException(4);
+        }
+        else{
+            fac.getEmployees().replace(empSecNum, emp);
+            return emp;
+        }
+    }
+
+    static void printEmployees(){
+        fac.getEmployees().forEach((key, value) -> {
+            value.toString();
+        });
+    }
+
+    static public void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String cmd = in.nextLine();
+        String[] cmd1 = cmd.split(" ");
+        while(!cmd1[0].equals("exit")){
+            switch(cmd1[0]){
+                case "get":
+                    try{
+                        Employee value = getEmployee(cmd1[1]);
+                        value.toString();
+                    }catch (EmployeeException e){
+                        System.out.println(e.code + "\n" + e.message);
                     }
-                }
-            }
-            else if(test1[0].equals("1")){
-                for(int j = 0; j < Integer.parseInt(n); j++){
-                    if(employees[j].getSocialSecurityNumber().equals(test1[1])){
-                        employees[j].toString();
+                    break;
+                case "add":
+                    Employee employee = null;
+                    switch(Integer.parseInt(cmd1[1])){
+                        case 0:
+                            employee = new SalaridEmployee(cmd1[2], cmd1[3], cmd1[4]);
+                            ((SalaridEmployee) employee).setWeeklySalary(Double.parseDouble(cmd1[5]));
+                            break;
+                        case 1:
+                            employee = new HourlyEmployee(cmd1[2], cmd1[3], cmd1[4]);
+                            ((HourlyEmployee) employee).setWage(Double.parseDouble(cmd1[5]));
+                            ((HourlyEmployee) employee).setHours(Double.parseDouble(cmd1[6]));
+                            break;
+                        case 2:
+                            employee = new CommisionEmployee(cmd1[2], cmd1[3], cmd1[4]);
+                            ((CommisionEmployee) employee).setGrossSales(Double.parseDouble(cmd1[5]));
+                            ((CommisionEmployee) employee).setCommissionRate(Double.parseDouble(cmd1[6]));
+                            break;
+                        case 3:
+                            employee = new basePlusCommisionEmployee(cmd1[2], cmd1[3], cmd1[4]);
+                            ((basePlusCommisionEmployee)employee).setGrossSales(Double.parseDouble(cmd1[5]));
+                            ((basePlusCommisionEmployee)employee).setCommissionRate(Double.parseDouble(cmd1[6]));
+                            ((basePlusCommisionEmployee)employee).setBaseSalary(Double.parseDouble(cmd1[7]));
                     }
-                }
+                    try{
+                        Employee value = addEmployee(employee);
+                        value.toString();
+                    }catch (EmployeeException e){
+                        System.out.println(e.code + "\n" + e.message);
+                    }
+                    break;
+                case "update":
+                    Employee employee1 = null;
+                    switch(Integer.parseInt(cmd1[1])){
+                        case 0:
+                            employee1 = new SalaridEmployee(cmd1[2], cmd1[3], cmd1[4]);
+                            ((SalaridEmployee) employee1).setWeeklySalary(Double.parseDouble(cmd1[5]));
+                            break;
+                        case 1:
+                            employee1 = new HourlyEmployee(cmd1[2], cmd1[3], cmd1[4]);
+                            ((HourlyEmployee) employee1).setWage(Double.parseDouble(cmd1[5]));
+                            ((HourlyEmployee) employee1).setHours(Double.parseDouble(cmd1[6]));
+                            break;
+                        case 2:
+                            employee1 = new CommisionEmployee(cmd1[2], cmd1[3], cmd1[4]);
+                            ((CommisionEmployee) employee1).setGrossSales(Double.parseDouble(cmd1[5]));
+                            ((CommisionEmployee) employee1).setCommissionRate(Double.parseDouble(cmd1[6]));
+                            break;
+                        case 3:
+                            employee1 = new basePlusCommisionEmployee(cmd1[2], cmd1[3], cmd1[4]);
+                            ((basePlusCommisionEmployee)employee1).setGrossSales(Double.parseDouble(cmd1[5]));
+                            ((basePlusCommisionEmployee)employee1).setCommissionRate(Double.parseDouble(cmd1[6]));
+                            ((basePlusCommisionEmployee)employee1).setBaseSalary(Double.parseDouble(cmd1[7]));
+                    }
+                    try{
+                        Employee value = updateEmployee(cmd1[4], employee1);
+                        value.toString();
+                    }catch (EmployeeException e){
+                        System.out.println(e.code + "\n" + e.message);
+                    }
+                    break;
+                case "delete":
+                    try{
+                        Employee value = deleteEmployee(cmd1[1]);
+                        value.toString();
+                    }catch (EmployeeException e){
+                        System.out.println(e.code + "\n" + e.message);
+                    }
+                    break;
+                case "print":
+                    printEmployees();
+                    break;
             }
+            cmd = in.nextLine();
+            cmd1 = cmd.split(" ");
         }
     }
 }
@@ -196,5 +279,36 @@ class basePlusCommisionEmployee extends CommisionEmployee{
 
     public void setBaseSalary(double baseSalary) {
         this.baseSalary = baseSalary;
+    }
+}
+
+class EmployeeException extends Exception{
+    String code;
+    String message;
+    EmployeeException(int n){
+        switch (n){
+            case 1:
+                code = "1004";
+                message = "employee not found.";
+                break;
+            case 2:
+                code = "1002";
+                message = "employee not found.";
+                break;
+            case 3:
+                code = "1001";
+                message = "employee exists.";
+                break;
+            case 4:
+                code = "1003";
+                message = "employee not found.";
+        }
+    }
+}
+
+class factory{
+    private TreeMap<String, Employee> employees = new TreeMap<>();
+    TreeMap<String, Employee> getEmployees(){
+        return employees;
     }
 }
